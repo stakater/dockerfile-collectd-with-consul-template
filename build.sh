fb4241b1788862e9ff62fe311a0a2be1148990fa
Ahmad Iqbal <ahmad@aurorasolutions.io>
2017-01-24 16:25:19 +0500

    changes for collectd

--- build.sh
+++ build.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 _consul_template_version=$1
-_logstash_with_consul_template_tag=$2
+_collectd_with_consul_template_tag=$2
 _release_build=false
 
 if [ -z "${_consul_template_version}" ]; then
 	source CONSUL_TEMPLATE_VERSION
 	_consul_template_version=$CONSUL_TEMPLATE_VERSION
-	_logstash_with_consul_template_tag=$CONSUL_TEMPLATE_VERSION
+	_collectd_with_consul_template_tag=$CONSUL_TEMPLATE_VERSION
 	_release_build=true
 fi
 
 echo "CONSUL_TEMPLATE_VERSION: ${_consul_template_version}"
-echo "DOCKER TAG: ${_logstash_with_consul_template_tag}"
+echo "DOCKER TAG: ${_collectd_with_consul_template_tag}"
 echo "RELEASE BUILD: ${_release_build}"
 
-docker build --build-arg CONSUL_TEMPLATE_VERSION=${_consul_template_version} --tag "stakater/logstash-with-consul-template:${_logstash_with_consul_template_tag}"  --no-cache=true .
+docker build --build-arg CONSUL_TEMPLATE_VERSION=${_consul_template_version} --tag "stakater/collectd-with-consul-template:${_collectd_with_consul_template_tag}"  --no-cache=true .
 
 if [ $_release_build == true ]; then
-	docker build --build-arg CONSUL_TEMPLATE_VERSION=${_consul_template_version} --tag "stakater/logstash-with-consul-template:latest"  --no-cache=true .
+	docker build --build-arg CONSUL_TEMPLATE_VERSION=${_consul_template_version} --tag "stakater/collectd-with-consul-template:latest"  --no-cache=true .
 fi
--- start.sh
+++ start.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-# We just start the logstash service and then leave consul-template running on the foreground.
+# We just start the collectd service and then leave consul-template running on the foreground.
 # Here we use the CONSUL_URL environment variable that we defined before. Consul template
-# expects to find a logstash.ctmpl file in /templates. This is the template that we would
-# mount as a volume from our host. The result is then placed in /etc/logstash/conf.d/logstash.conf
-# where logstash will be able to read from.
-# Logstash is run with `-r` flag which reloads logstash if config is changed, consul only updates
-# the configuration file and Logstash itself reads it.
-# Note: & added so that logstash runs in a separate process and consul-template runs in this process
-logstash -f /etc/logstash/conf.d/logstash.conf -r &
-consul-template -consul=$CONSUL_URL -template="/templates/logstash.ctmpl:/etc/logstash/conf.d/logstash.conf"
+# expects to find a collectd.ctmpl file in /templates. This is the template that we would
+# mount as a volume from our host. The result is then placed in /etc/collectd/conf.d/collectd.conf
+# where collectd will be able to read from.
+# collectd will run in a separate process and consul-template runs in this process
+kill -9 `collectd.pid`
+consul-template -consul=$CONSUL_URL -template="/templates/collectd.ctmpl:/etc/collectd/conf.d/collectd.conf:collectd -C /etc/collectd/conf.d/collectd.conf -P collectd.pid"
